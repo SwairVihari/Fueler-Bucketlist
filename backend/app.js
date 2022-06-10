b872eced1790f69106314c732c171164263703dd
harsh patel <harsh.1722.patel@gmail.com>
2022-06-10 20:43:03 +0530

    prod

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cookieParser = require('cookie-parser')
 const errorMiddleware = require("./middleware/error")
+const path = require("path")
 
 app.use(express.json())
 app.use(cookieParser())
@@ -11,6 +12,14 @@ const test = require("./routes/testRoute")
 const user = require("./routes/userRoute") 
 app.use("/api/v1", test)
 app.use("/api/v1", user)
+
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
+
+
 
 // Middleware for Errors
 app.use(errorMiddleware)
